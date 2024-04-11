42438480fe292e0f5bd5600288131ce3e475b9f0
David Arteaga <david.arteaga@revinate.com>
2024-04-11 15:56:01 -0300

    Modify env inputs required to execute a scan on vms cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,4 @@ echo "::notice file=entrypoint.sh,line=7::$GREETING"
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
-python3 /app/main.py
-
-exit 0
+python3 /app/main.py --api_key=$API_KEY --ecosystem=$ECOSYSTEM --client_name=$CLIENT_NAME --project=$PROJECT_NAME
